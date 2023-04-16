5a32953302c7e61f6403c73b573a9b191566c7eb
Alexander <Lisnevskiy14@gmail.com>
2023-04-16 19:36:40 +0300

    Added function capitalize_first_letter()

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def make_all_letters_uppercase(string: str):
     return string.upper()
+
+
+def capitalize_first_letter(string: str):
+    """
+    Принимает строку и делает заглавными первые буквы каждого слова.
+    :param string: str
+    :return: str
+    """
+    return string.title()
